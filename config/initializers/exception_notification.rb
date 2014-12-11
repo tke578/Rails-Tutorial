8db5dc15b41f8922bb124e8a1fb25754a3c87553
Drew OG <tke578@gmail.com>
2014-12-11 09:02:09 +0000

    fix

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.config.middleware.use ExceptionNotification::Rack,
   :email => {
     :email_prefix => "[sampleApp] ",
-    :sender_address => %{"notifier" <notifier@example.com>},
-    :exception_recipients => %w{xyz@example.com}
+    :sender_address => %{"notifier" <andrew@sfrent.net>},
+    :exception_recipients => %w{andrew@sfrent.net}
   }
