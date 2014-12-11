89a4a387844ef0074015e87b918845fba30f7a81
Drew OG <tke578@gmail.com>
2014-12-11 09:11:15 +0000

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,33 +77,23 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   host = 'pandulce.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => 'smtp.sendgrid.net',
+  #   :port           => '587',
+  #   :authentication => :plain,
+  #   :user_name      => ENV['SENDGRID_USERNAME'],
+  #   :password       => ENV['SENDGRID_PASSWORD'],
+  #   :domain         => 'heroku.com',
+  #   :enable_starttls_auto => true
   }
   
-  # config.middleware.use ExceptionNotification::Rack,
-  # :email => {
-  #   :email_prefix => "[Whatever] ",
-  #   :sender_address => %{"notifier" <notifier@example.com>},
-  #   :exception_recipients => %w{andrew@sfrent.net},
-  #   :delivery_method => :smtp,
-  #   :smtp_settings => {
-  #     :address        => 'smtp.sendgrid.net',
-  #     :port           => '587',
-  #     :authentication => :plain,
-  #     :user_name      => ENV['SENDGRID_USERNAME'],
-  #     :password       => ENV['SENDGRID_PASSWORD'],
-  #     :domain         => 'heroku.com',
-  #     :enable_starttls_auto => true
-  #   }
-  # }
+  SampleApp::Application.config.middleware.use ExceptionNotification::Rack,
+  :email => {
+    :email_prefix => "[Whatever] ",
+    :sender_address => %{"notifier" <notifier@example.com>},
+    :exception_recipients => %w{andrew@sfrent.net},
+  }
 end
